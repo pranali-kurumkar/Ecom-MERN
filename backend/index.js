653e01ff70ce9c25bb63268d2f1483d920e3a392
pranali-kurumkar <pranalikurumkar07@gmail.com>
2025-04-15 00:23:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -51,7 +51,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     if (req.file) {
         res.json({
             success: 1,
-            image_url: `https://e-commerce-backend-53cq.onrender.com/images/${req.file.filename}`
+            image_url: `https://ecom-mern-admin.onrender.com/images/${req.file.filename}`
         });
     } 
     else {
